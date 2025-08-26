3843cbfda45fd85563d0c4403d950250435fa7f7
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-08-26 18:51:15 +0200

    feat: chicken in "chicken.class.js" adjusted

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -1,7 +1,8 @@
 class Chicken extends MovableObjects {
-  y = 295;
+  y = 292;
   constructor() {
     super().loadImage("assets/imgs/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
     this.x = 200 + Math.random() * 500;
+    this.y = 283 + Math.random() * 10;
   }
 }
