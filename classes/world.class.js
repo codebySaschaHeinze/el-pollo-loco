c2c127ed1b9c3df4240e8dccca93957780d4adf3
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-08-27 16:39:19 +0200

    feat: folder "levels" added, file "level1" added.

--- classes/world.class.js
+++ classes/world.class.js
@@ -1,26 +1,8 @@
 class World {
   character;
-  enemies = [new Chicken(), new Chicken(), new Chicken()];
-
-  clouds = [new Cloud()];
-
-  backgroundObjects = [
-    new BackgroundObjects("assets/imgs/5_background/layers/air.png", 0),
-    new BackgroundObjects("assets/imgs/5_background/layers/3_third_layer/full.png", 0),
-    new BackgroundObjects("assets/imgs/5_background/layers/2_second_layer/full.png", 0),
-    new BackgroundObjects("assets/imgs/5_background/layers/1_first_layer/full.png", 0),
-
-    new BackgroundObjects("assets/imgs/5_background/layers/air.png", 1440),
-    new BackgroundObjects("assets/imgs/5_background/layers/3_third_layer/full.png", 1440),
-    new BackgroundObjects("assets/imgs/5_background/layers/2_second_layer/full.png", 1440),
-    new BackgroundObjects("assets/imgs/5_background/layers/1_first_layer/full.png", 1440),
-
-    new BackgroundObjects("assets/imgs/5_background/layers/air.png", 2880),
-    new BackgroundObjects("assets/imgs/5_background/layers/3_third_layer/full.png", 2880),
-    new BackgroundObjects("assets/imgs/5_background/layers/2_second_layer/full.png", 2880),
-    new BackgroundObjects("assets/imgs/5_background/layers/1_first_layer/full.png", 2880),
-  ];
-
+  enemies = level1.enemies;
+  clouds = level1.clouds;
+  backgroundObjects = level1.backgroundObjects;
   canvas;
   ctx;
   keyboard;
