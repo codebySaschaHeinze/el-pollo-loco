48ba2dd8288d394a7dcb0d9b49bfc9b2fd51d65b
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-08-27 20:09:01 +0200

    refactor: script.js

--- script.js
+++ script.js
@@ -11,17 +11,17 @@ function init() {
 }
 
 window.addEventListener("keydown", (event) => {
-  if (event.key === "ArrowLeft") keyboard.LEFT = true;
-  if (event.key === "ArrowUp") keyboard.UP = true;
-  if (event.key === "ArrowRight") keyboard.RIGHT = true;
-  if (event.key === "ArrowDown") keyboard.DOWN = true;
+  if (event.key === "ArrowLeft" || event.key === "a" || event.key === "A") keyboard.LEFT = true;
+  if (event.key === "ArrowUp" || event.key === "w" || event.key === "W") keyboard.UP = true;
+  if (event.key === "ArrowRight" || event.key === "d" || event.key === "D") keyboard.RIGHT = true;
+  if (event.key === "ArrowDown" || event.key === "s" || event.key === "S") keyboard.DOWN = true;
   if (event.key === " ") keyboard.SPACE = true;
 });
 
 window.addEventListener("keyup", (event) => {
-  if (event.key === "ArrowLeft") keyboard.LEFT = false;
-  if (event.key === "ArrowUp") keyboard.UP = false;
-  if (event.key === "ArrowRight") keyboard.RIGHT = false;
-  if (event.key === "ArrowDown") keyboard.DOWN = false;
+  if (event.key === "ArrowLeft" || event.key === "a" || event.key === "A") keyboard.LEFT = false;
+  if (event.key === "ArrowUp" || event.key === "w" || event.key === "W") keyboard.UP = false;
+  if (event.key === "ArrowRight" || event.key === "d" || event.key === "D") keyboard.RIGHT = false;
+  if (event.key === "ArrowDown" || event.key === "s" || event.key === "S") keyboard.DOWN = false;
   if (event.key === " ") keyboard.SPACE = false;
 });
