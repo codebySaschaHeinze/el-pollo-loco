4c5d9846e498d52b6d887b7f4f49c76d498c53bb
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-09-01 11:19:26 +0200

    feat: level1 is now 12000px wide

--- levels/level1.js
+++ levels/level1.js
@@ -1,20 +1,69 @@
-const level1 = new Level(
-  [new Chicken(), new Chicken(), new Chicken(), new Endboss()],
-  [new Cloud(720, 100), new Cloud(720, 400), new Cloud(720, 800), new Cloud(720, 1200)],
-  [
-    new BackgroundObjects("assets/imgs/5_background/layers/air.png", -100),
-    new BackgroundObjects("assets/imgs/5_background/layers/3_third_layer/full.png", -100),
-    new BackgroundObjects("assets/imgs/5_background/layers/2_second_layer/full.png", -100),
-    new BackgroundObjects("assets/imgs/5_background/layers/1_first_layer/full.png", -100),
+const LEVEL_WIDTH = 12000;
+const endboss = new Endboss();
+endboss.x = LEVEL_WIDTH + 350; // ggf. feintunen
 
-    new BackgroundObjects("assets/imgs/5_background/layers/air.png", 1340),
-    new BackgroundObjects("assets/imgs/5_background/layers/3_third_layer/full.png", 1340),
-    new BackgroundObjects("assets/imgs/5_background/layers/2_second_layer/full.png", 1340),
-    new BackgroundObjects("assets/imgs/5_background/layers/1_first_layer/full.png", 1340),
+const clouds = [
+  new Cloud(LEVEL_WIDTH, 100),
+  new Cloud(LEVEL_WIDTH, 1200),
+  new Cloud(LEVEL_WIDTH, 2400),
+  new Cloud(LEVEL_WIDTH, 3600),
+  new Cloud(LEVEL_WIDTH, 5000),
+  new Cloud(LEVEL_WIDTH, 6500),
+  new Cloud(LEVEL_WIDTH, 8000),
+  new Cloud(LEVEL_WIDTH, 9500),
+  new Cloud(LEVEL_WIDTH, 10800),
+];
 
-    new BackgroundObjects("assets/imgs/5_background/layers/air.png", 2780),
-    new BackgroundObjects("assets/imgs/5_background/layers/3_third_layer/full.png", 2780),
-    new BackgroundObjects("assets/imgs/5_background/layers/2_second_layer/full.png", 2780),
-    new BackgroundObjects("assets/imgs/5_background/layers/1_first_layer/full.png", 2780),
-  ]
-);
+const backgrounds = [
+  new BackgroundObjects("assets/imgs/5_background/layers/air.png", -100),
+  new BackgroundObjects("assets/imgs/5_background/layers/3_third_layer/full.png", -100),
+  new BackgroundObjects("assets/imgs/5_background/layers/2_second_layer/full.png", -100),
+  new BackgroundObjects("assets/imgs/5_background/layers/1_first_layer/full.png", -100),
+
+  new BackgroundObjects("assets/imgs/5_background/layers/air.png", 1340),
+  new BackgroundObjects("assets/imgs/5_background/layers/3_third_layer/full.png", 1340),
+  new BackgroundObjects("assets/imgs/5_background/layers/2_second_layer/full.png", 1340),
+  new BackgroundObjects("assets/imgs/5_background/layers/1_first_layer/full.png", 1340),
+
+  new BackgroundObjects("assets/imgs/5_background/layers/air.png", 2780),
+  new BackgroundObjects("assets/imgs/5_background/layers/3_third_layer/full.png", 2780),
+  new BackgroundObjects("assets/imgs/5_background/layers/2_second_layer/full.png", 2780),
+  new BackgroundObjects("assets/imgs/5_background/layers/1_first_layer/full.png", 2780),
+
+  new BackgroundObjects("assets/imgs/5_background/layers/air.png", 4220),
+  new BackgroundObjects("assets/imgs/5_background/layers/3_third_layer/full.png", 4220),
+  new BackgroundObjects("assets/imgs/5_background/layers/2_second_layer/full.png", 4220),
+  new BackgroundObjects("assets/imgs/5_background/layers/1_first_layer/full.png", 4220),
+
+  new BackgroundObjects("assets/imgs/5_background/layers/air.png", 5660),
+  new BackgroundObjects("assets/imgs/5_background/layers/3_third_layer/full.png", 5660),
+  new BackgroundObjects("assets/imgs/5_background/layers/2_second_layer/full.png", 5660),
+  new BackgroundObjects("assets/imgs/5_background/layers/1_first_layer/full.png", 5660),
+
+  new BackgroundObjects("assets/imgs/5_background/layers/air.png", 7100),
+  new BackgroundObjects("assets/imgs/5_background/layers/3_third_layer/full.png", 7100),
+  new BackgroundObjects("assets/imgs/5_background/layers/2_second_layer/full.png", 7100),
+  new BackgroundObjects("assets/imgs/5_background/layers/1_first_layer/full.png", 7100),
+
+  new BackgroundObjects("assets/imgs/5_background/layers/air.png", 8540),
+  new BackgroundObjects("assets/imgs/5_background/layers/3_third_layer/full.png", 8540),
+  new BackgroundObjects("assets/imgs/5_background/layers/2_second_layer/full.png", 8540),
+  new BackgroundObjects("assets/imgs/5_background/layers/1_first_layer/full.png", 8540),
+
+  new BackgroundObjects("assets/imgs/5_background/layers/air.png", 9980),
+  new BackgroundObjects("assets/imgs/5_background/layers/3_third_layer/full.png", 9980),
+  new BackgroundObjects("assets/imgs/5_background/layers/2_second_layer/full.png", 9980),
+  new BackgroundObjects("assets/imgs/5_background/layers/1_first_layer/full.png", 9980),
+
+  new BackgroundObjects("assets/imgs/5_background/layers/air.png", 11420),
+  new BackgroundObjects("assets/imgs/5_background/layers/3_third_layer/full.png", 11420),
+  new BackgroundObjects("assets/imgs/5_background/layers/2_second_layer/full.png", 11420),
+  new BackgroundObjects("assets/imgs/5_background/layers/1_first_layer/full.png", 11420),
+
+  new BackgroundObjects("assets/imgs/5_background/layers/air.png", 12860),
+  new BackgroundObjects("assets/imgs/5_background/layers/3_third_layer/full.png", 12860),
+  new BackgroundObjects("assets/imgs/5_background/layers/2_second_layer/full.png", 12860),
+  new BackgroundObjects("assets/imgs/5_background/layers/1_first_layer/full.png", 12860),
+];
+
+const level1 = new Level([new Chicken(), new Chicken(), new Chicken(), endboss], clouds, backgrounds);
