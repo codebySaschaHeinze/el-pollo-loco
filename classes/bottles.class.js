abbb0f28cb725d391caf09fca92a032ed151c06c
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-09-02 16:26:20 +0200

    feat: "bottles.class.js" added

---
+++ classes/bottles.class.js
@@ -0,0 +1,18 @@
+class Bottles extends MovableObjects {
+  constructor(x, y) {
+    super().loadImage("assets/imgs/6_salsa_bottle/1_salsa_bottle_on_ground.png");
+    this.x = x;
+    this.y = y;
+    this.width = 30;
+    this.height = 40;
+    this.offset = { top: 6, right: 6, bottom: 6, left: 6 };
+    this.placed = false;
+  }
+  draw(ctx) {
+    if (!this.placed && this.world && this.world.character) {
+      const ground = this.world.character.groundBottom || 417;
+      this.y = ground - this.height;
+      this.placed = true;
+    }
+  }
+}
